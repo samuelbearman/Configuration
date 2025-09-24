7e84aef23a3cd3df16c7afe39f2d393e5a4d63cb
Samuel Bearman <samuel.opensource@gmail.com>
2025-09-23 22:11:31 -0400

    Windbg scripts

---
+++ windbg/personal_extensions.js
@@ -0,0 +1,78 @@
+/// <reference path="JSProvider.d.ts" />
+
+"use strict";
+
+const execute = cmd => host.namespace.Debugger.Utility.Control.ExecuteCommand(cmd);
+const logln  = msg => host.diagnostics.debugLog(`[+] ${msg}\n`);
+
+function _getEprocess(cmd) {
+
+    let Control = host.namespace.Debugger.Utility.Control;
+
+    for(let Line of Control.ExecuteCommand(cmd)) {
+        if(Line.includes("PROCESS")) {
+            let split = Line.split(' ');
+            return split[1];
+        }
+    }
+
+    return '';
+
+}
+
+function _getThread(eprocess) {
+    let Control = host.namespace.Debugger.Utility.Control;
+
+    let cmd = `!process ${eprocess}`
+
+    var threadRegex = /THREAD\s+([0-9a-fA-F`]+)/;
+
+    for(let Line of Control.ExecuteCommand(cmd)) {
+        var m = threadRegex.exec(Line);
+        if(m && m[1]) {
+            let threadClean = m[1].replace(/^0x/, '');
+            logln('THREAD -> ' + m + ' -> ' + threadClean);
+            return threadClean;
+        }
+    }
+}
+ 
+// Exported function
+function attachusermode(procName) {
+    if (!procName || procName.length === 0) {
+        logln("Usage: !attachusermode (\"processname.exe\")\n");
+        return { error: "missing process name parameter" };
+    }
+
+    var control = host.namespace.Debugger.Utility.Control;
+
+    // 1) !process 0 0 <procName>
+    var cmd1 = "!process 0 0 " + procName;
+    logln("Executing: " + cmd1 + "\n");
+    let eprocess = _getEprocess(cmd1);
+
+    if(eprocess === '') {
+        return { error: "EPROCESS not found" };
+    }
+
+    logln('EPROCESS -> ' + eprocess);
+
+    execute(`.process /i ${eprocess}`)
+    execute('g')
+
+    let ethread = _getThread(eprocess)
+
+    logln("ETHREAD -> " + ethread)
+    execute(`.thread /w ${ethread}`)
+    execute('.reload /f /user')
+}
+
+function initializeScript() {
+    return [
+        new host.apiVersionSupport(1, 3),
+        new host.functionAlias(
+            attachusermode,
+            'attachusermode'
+        ),
+    ];
+}
