ba12cfc68451b50d0b56701c9be5bfc51ff908f2
Samuel Bearman <samuel.opensource@gmail.com>
2025-11-16 22:49:19 -0500

    vim, tools, etc.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Configuration
+
+Profiles, tools, and other personal things that I like.
+
+## Other notes
+
+- Prefered Font is JetBrains Mono Nerd Font (https://www.nerdfonts.com/font-downloads)
--- powershell-profile.ps1
+++ powershell-profile.ps1
@@ -2,3 +2,11 @@ Del Alias:ls
 Set-Alias -Name ls -Value "C:\bin\eza"
 
 oh-my-posh init pwsh --config $env:POSH_THEMES_PATH\apple.omp.json | Invoke-Expression
+
+function LL {
+    & "C:\bin\eza.exe" -la @args
+}
+
+function fzfp {
+    fzf --preview "bat --color=always --style=numbers --line-range=:500 {}" 
+}
